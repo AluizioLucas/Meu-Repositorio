3bcc52ad3cb05b8faad06c3e4d58aa5577b0d1ad
Aluiziolucas <aluiziolucas@ali.ufc.br>
2020-12-27 06:22:50 -0300

    Projeto semanal ArrayLIst

--- Projeto_ArrayList/JunkFoodMachine.java
+++ Projeto_ArrayList/JunkFoodMachine.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Espiral{
     public String nome = "-";
@@ -19,7 +20,7 @@ class Espiral{
 
     @Override
     public String toString() {
-        return qtd + " : " + nome + " : " + valor;
+        return "Unidades : " + qtd + " : " + nome + " : " + valor + "$";
     }
 }
 
@@ -31,31 +32,54 @@ public class JunkFoodMachine {
     public Float lucro;
     public ArrayList<Espiral> espiral;
 
+    public JunkFoodMachine(){
+        this.saldo = 0;
+        this.lucro = 0.0f;
+        this.espiral = new ArrayList<>();
+    }
+
     public void addDinheiro(Float dindin){
         this.saldo = dindin;
     }
 
+    public void addProduto(String nome, float valor, int qtd){
+        espiral.add(new Espiral(nome, valor, qtd));
+    }
+
     public void troco(){
         if(saldo > 0){
-            System.out.println("System: Seu troco é de " + saldo);
+            System.out.println("System: Seu troco é de " + saldo + "$");
             this.saldo = 0;
         }
         else System.out.println("System: não há troco");
     }
 
-    public boolean vender(int indice){
-        if(indice >= 0 && indice < espiral.size()){
-            if(espiral.get(indice).valor <= this.saldo){
-                this.saldo -= espiral.get(indice).valor;
-                this.lucro += espiral.get(indice).valor;
-                espiral.get(indice).qtd --;
-                return true;
+    public void comprar(int indice, int qtd){
+        for(int i = 0; i < qtd; i++){
+            if(indice >= 0 && indice < espiral.size()){
+                if(espiral.get(indice).valor <= this.saldo){
+                    if(espiral.get(indice).qtd > 0){
+                        this.saldo -= espiral.get(indice).valor;
+                        this.lucro += espiral.get(indice).valor;
+                        espiral.get(indice).qtd --;
+                    }
+                    else {
+                        System.out.println("SYSTEM-FAIL:Produto insuficiente");
+                        break;
+                    }
+                }
+                else {
+                    System.out.println("SYSTEM-FAIL:Saldo insuficiente");
+                    break;
+                }
             }
-            System.out.println("SYSTEM-FAIL:Saldo insuficiente");
-            return false;
+            else {
+                System.out.println("SYSTEM-FAIL:Indice não existe");
+                break;
+            }
+            
         }
-        System.out.println("SYSTEM-FAIL:Indice não existe");
-        return false;
+        
     }
 
     public boolean alterarEspiral(int indice, String nome, float valor, int qtd){
@@ -72,18 +96,70 @@ public class JunkFoodMachine {
 
     @Override
     public String toString() {
-        String saida = "System info-----------\n";
-       for(int i=0; i<=espiral.size(); i++){
-           saida += i + " [ " + espiral.get(i).nome + " : " + espiral.get(i).qtd + " : " + espiral.get(i).valor+ " ]\n";
-       }
+        String saida = "Saldo : " + this.saldo + "\n";
+        int i=0;
+        for(Espiral espira : espiral){
+            saida += i +"- [ " + espira + " ] \n";
+            i++;
+        }
         return saida;
     }
 
+  
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        JunkFoodMachine maquina1 = new JunkFoodMachine();
         
+        System.out.println("COMANDOS----------------/n");
+        System.out.println("Iniciar\nEncerrar\nAddDinheiro qtd\nAddProduto nome valor qtd\ntroco\nComprar indice qtd\nSetEspiral indice nome valor qtd\nStatus\n");
 
+        while(true){
+            String line = scanner.nextLine();
+            String[] ui = line.split(" ");
 
-        
+            if(ui[0].equals("Iniciar")){
+                maquina1 = new JunkFoodMachine();
+                System.out.println(maquina1);
+            }
+
+            if(ui[0].equals("Encerrar")){
+                System.out.println("Fim do programa");
+                break;
+            }
+
+            if(ui[0].equals("AddDinheiro")){
+                maquina1.addDinheiro(Float.parseFloat(ui[1]));
+                System.out.println(maquina1);
+            }
+
+            if(ui[0].equals("AddProduto")){
+                maquina1.addProduto(ui[1], Float.parseFloat(ui[2]), Integer.parseInt(ui[3]));
+                System.out.println(maquina1);
+            }
+            
+            if(ui[0].equals("troco")){
+                maquina1.troco();
+            }
+
+            if(ui[0].equals("Comprar")){
+                maquina1.comprar(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]));
+                System.out.println(maquina1);
+            }
+
+            if(ui[0].equals("SetEspiral")){
+                maquina1.alterarEspiral(Integer.parseInt(ui[1]), ui[2], Float.parseFloat(ui[3]), Integer.parseInt(ui[4]));
+                System.out.println(maquina1);
+            }
+
+            if(ui[0].equals("Status")){
+                System.out.println(maquina1);
+            }
+
+            
+        }
+        scanner.close();
+    
     }
 
 
